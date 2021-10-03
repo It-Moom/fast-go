9e56327a4500c28d38429a1cfdb6ab68d5abd292
GabbyMrH <gabbymrh@gmail.com>
2021-10-03 23:42:52 +0800

    feat:增加安全工具及其单元测试

---
+++ tests/secure_util_test.go
@@ -0,0 +1,28 @@
+/*
+ * @PackageName: tests
+ * @Description: 安全工具单元测试
+ * @Author: gabbymrh
+ * @Date: 2021-10-03 23:28:51
+ * @Last Modified by: gabbymrh
+ * @Last Modified time: 2021-10-03 23:28:51
+ */
+
+package tests
+
+import (
+	"fast-go/pkg/utils/secure"
+	"testing"
+)
+
+// 测试生成密码
+func TestGeneratePassword(t *testing.T) {
+	password := secure.GeneratePassword("123456")
+	t.Log(password) // 如:$2a$10$caUw3B4E1JmevafFax6UyuI7OqRW4AJlWxlhvCptlTAG0fvqLDS6m
+}
+
+// 测试校验密码
+func TestVerifyPassword(t *testing.T) {
+	hashedPassword := "$2a$10$caUw3B4E1JmevafFax6UyuI7OqRW4AJlWxlhvCptlTAG0fvqLDS6m"
+	passwordVerify := secure.VerifyPassword("123456", hashedPassword)
+	t.Log(passwordVerify) // 如：true
+}
