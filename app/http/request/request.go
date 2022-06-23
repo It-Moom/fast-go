84e91f6865e183deef6ed1da5890fde52920cb07
GabbyMrH <gabbymrh@gmail.com>
2022-06-23 12:35:36 +0800

    feat:增加参数验证基础文件

---
+++ app/http/request/request.go
@@ -0,0 +1,68 @@
+/*
+ * @PackageName: request
+ * @Description: request请求处理
+ * @Author: gabbymrh
+ * @Date: 2022-06-23 12:30:15
+ * @LastModifiedBy: gabbymrh
+ * @LastModifiedAt: 2022-06-23 12:30:15
+ */
+
+package request
+
+import (
+	"fast-go/app/handler/http_response"
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+)
+
+// ValidatorFunc 验证函数类型
+type ValidatorFunc func(interface{}, *gin.Context) map[string][]string
+
+// Validate 控制器里调用示例：
+//        if ok := requests.Validate(c, &requests.UserSaveRequest{}, requests.UserSave); !ok {
+//            return
+//        }
+func Validate(c *gin.Context, obj interface{}, handler ValidatorFunc) bool {
+
+	// 1. 解析请求，支持 JSON 数据、表单请求和 URL Query
+	if err := c.ShouldBind(obj); err != nil {
+		// 创建 []string 类型的错误信息
+		errors := []string{"请求解析错误，请确认请求格式是否正确。上传文件请使用 multipart 标头，参数请使用 JSON 格式。"}
+		http_response.RequestError(c, err, errors)
+		return false
+	}
+
+	// 2. 表单验证
+	errs := handler(obj, c)
+
+	// 3. 判断验证是否通过
+	if len(errs) > 0 {
+		http_response.ParamError(c, errs)
+		return false
+	}
+
+	return true
+}
+
+func validate(data interface{}, rules govalidator.MapData, messages govalidator.MapData) map[string][]string {
+	// 配置选项
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
+		Messages:      messages,
+	}
+	// 开始验证
+	return govalidator.New(opts).ValidateStruct()
+}
+
+func validateFile(c *gin.Context, data interface{}, rules govalidator.MapData, messages govalidator.MapData) map[string][]string {
+	opts := govalidator.Options{
+		Request:       c.Request,
+		Rules:         rules,
+		Messages:      messages,
+		TagIdentifier: "valid",
+	}
+	// 调用 govalidator 的 Validate 方法来验证文件
+	return govalidator.New(opts).Validate()
+}
