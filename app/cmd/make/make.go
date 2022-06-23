17a18158fbf5efd05a7ce5753f23138e437e0e1c
GabbyMrH <gabbymrh@gmail.com>
2022-06-23 10:52:21 +0800

    feat:移除复数变化

--- app/cmd/make/make.go
+++ app/cmd/make/make.go
@@ -24,39 +24,29 @@ import (
 // 单个词，用户命令传参，以 User 模型为例：
 //     - user
 //  - User
-//  - users
-//  - Users
 // 整理好的数据：
 // {
-//     "TableName": "users",
+//     "TableName": "user",
 //     "StructName": "User",
-//     "StructNamePlural": "Users"
 //     "VariableName": "user",
-//     "VariableNamePlural": "users",
 //     "PackageName": "user"
 // }
 // -
 // 两个词或者以上，用户命令传参，以 TopicComment 模型为例：
 //     - topic_comment
-//  - topic_comments
 //  - TopicComment
-//  - TopicComments
 // 整理好的数据：
 // {
-//     "TableName": "topic_comments",
+//     "TableName": "topic_comment",
 //     "StructName": "TopicComment",
-//     "StructNamePlural": "TopicComments"
 //     "VariableName": "topicComment",
-//     "VariableNamePlural": "topicComments",
 //     "PackageName": "topic_comment"
 // }
 type Model struct {
-	TableName          string
-	StructName         string
-	StructNamePlural   string
-	VariableName       string
-	VariableNamePlural string
-	PackageName        string
+	TableName    string
+	StructName   string
+	VariableName string
+	PackageName  string
 }
 
 // stubsFS 方便我们后面打包这些 .stub 为后缀名的文件
@@ -82,11 +72,9 @@ func init() {
 func makeModelFromString(name string) Model {
 	model := Model{}
 	model.StructName = str.Singular(strcase.ToCamel(name))
-	model.StructNamePlural = str.Plural(model.StructName)
-	model.TableName = str.Snake(model.StructNamePlural)
+	model.TableName = str.Snake(model.StructName)
 	model.VariableName = str.LowerCamel(model.StructName)
 	model.PackageName = str.Snake(model.StructName)
-	model.VariableNamePlural = str.LowerCamel(model.StructNamePlural)
 	return model
 }
 
@@ -114,9 +102,7 @@ func createFileFromStub(filePath string, stubName string, model Model, variables
 
 	// 添加默认的替换变量
 	replaces["{{VariableName}}"] = model.VariableName
-	replaces["{{VariableNamePlural}}"] = model.VariableNamePlural
 	replaces["{{StructName}}"] = model.StructName
-	replaces["{{StructNamePlural}}"] = model.StructNamePlural
 	replaces["{{PackageName}}"] = model.PackageName
 	replaces["{{TableName}}"] = model.TableName
 
