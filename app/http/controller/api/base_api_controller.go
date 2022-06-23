0ff6ac6334212fb5986826840f910d07187cd292
GabbyMrH <gabbymrh@gmail.com>
2022-06-23 18:08:07 +0800

    feat:新增基础api控制器

---
+++ app/http/controller/api/base_api_controller.go
@@ -0,0 +1,14 @@
+/*
+ * @PackageName: api
+ * @Description: 基础 API 控制器
+ * @Author: gabbymrh
+ * @Date: 2022-06-23 18:06:31
+ * @LastModifiedBy: gabbymrh
+ * @LastModifiedAt: 2022-06-23 18:06:31
+ */
+
+package api
+
+// BaseAPIController 基础控制器
+type BaseAPIController struct {
+}
