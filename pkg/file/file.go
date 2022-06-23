473ad7c3bcf87ba02184a16aaf7d07c1b7fdf4fb
GabbyMrH <gabbymrh@gmail.com>
2022-06-23 19:49:33 +0800

    feat: 封装文件上传功能

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -10,7 +10,13 @@
 package file
 
 import (
+	"fast-go/pkg/app"
+	"fast-go/pkg/auth"
+	"fast-go/pkg/helpers"
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"io/ioutil"
+	"mime/multipart"
 	"os"
 	"path/filepath"
 	"strings"
@@ -36,4 +42,28 @@ func Exists(fileToCheck string) bool {
 // FileNameWithoutExtension 获取文件名，不包含扩展名
 func FileNameWithoutExtension(fileName string) string {
 	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
+}
+
+// SaveUploadFile 保存上传的头像
+func SaveUploadFile(c *gin.Context, file *multipart.FileHeader) (string, error) {
+
+	var avatar string
+	// 确保目录存在，不存在创建
+	publicPath := "public"
+	dirName := fmt.Sprintf("/uploads/%s/%s/", app.TimenowInTimezone().Format("2006/01/02"), auth.CurrentUID(c))
+	os.MkdirAll(publicPath+dirName, 0755)
+
+	// 保存文件
+	fileName := randomNameFromUploadFile(file)
+	// public/uploads/2021/12/22/1/nFDacgaWKpWWOmOt.png
+	avatarPath := publicPath + dirName + fileName
+	if err := c.SaveUploadedFile(file, avatarPath); err != nil {
+		return avatar, err
+	}
+
+	return avatarPath, nil
+}
+
+func randomNameFromUploadFile(file *multipart.FileHeader) string {
+	return helpers.RandomString(16) + filepath.Ext(file.Filename)
 }
