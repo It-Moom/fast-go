35a00b555a832bdc887cbc2a9d6a886d4ff971f9
GabbyMrH <gabbymrh@gmail.com>
2022-06-24 10:00:47 +0800

    feat: 新增封装 make service 命令用于自动生成接口及其实现类文件

--- app/cmd/make/make.go
+++ app/cmd/make/make.go
@@ -72,6 +72,7 @@ func init() {
 		CmdMakeFactory,
 		CmdMakeSeeder,
 		CmdMakePolicy,
+		CmdMakeService,
 	)
 }
 
---
+++ app/constant/system/system.go
@@ -0,0 +1,16 @@
+/*
+ * @PackageName: system
+ * @Description: 系统常量
+ * @Author: gabbymrh
+ * @Date: 2022-06-24 09:32:15
+ * @LastModifiedBy: gabbymrh
+ * @LastModifiedAt: 2022-06-24 09:32:15
+ */
+
+package system
+
+// SERVICE_INTF_DIR_NAME 服务接口文件夹名称
+const SERVICE_INTF_DIR_NAME = "intf"
+
+// SERVICE_IMPL_DIR_NAME 服务实现文件夹名称
+const SERVICE_IMPL_DIR_NAME = "impl"
